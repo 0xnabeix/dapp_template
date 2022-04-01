0bbfa82750ae0a2101f1f85560c4b205d4796e42
0xsillyfish <santamiura@gmail.com>
2022-04-01 21:07:27 +0900

    add web3 ethers

---
+++ src/layout/Header.js
@@ -0,0 +1,24 @@
+import Web3Modal from 'web3modal';
+import providerOptions from '../utils/Web3ProviderOptions';
+
+const Header = () => {
+  const handleClick = () => {
+    try {
+      const web3Modal = new Web3Modal({
+          providerOptions
+      });
+      const provider = await web3Modal.connect();
+      // await activate(provider);
+      // activateBrowserWallet();
+      // console.log("account", account)
+    } catch(e) {
+        console.log(e);
+    }
+  }
+
+  return (
+    <button onClick={handleClick}>Connect wallet</button>
+  )
+}
+
+export default;
--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,3 +1,5 @@
+import Header from "./Header";
+
 const MainLayout = ({children}) => {
   return (
     <>
